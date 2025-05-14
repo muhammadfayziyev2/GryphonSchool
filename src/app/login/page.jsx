7d6f891a9bda44ed4e67e559df6c1be72d90b55c
muhammad <fajzievmuhammad09@gmail.com>
2025-05-14 17:40:09 +0500

    modified:   src/app/globals.css 	modified:   src/app/login/page.jsx

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className='login-unit'>
+      <div className="contact-form-container">
+        <h2>Войти</h2>
+        <form className="contact-form">
+          <label htmlFor="">Фамилия и имя</label>
+          <input type="text" placeholder="Фамилия и имя" name="name" required />
+          <label htmlFor="">Номер телефона</label>
+          <input type="text" placeholder="Номер телефона" name="email" required />
+     <label htmlFor="">Пароль</label>
+          <input type="password" placeholder="Пароль" name="subject" required />
+        
+          <button type="submit">Send Message</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
